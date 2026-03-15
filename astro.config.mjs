1205c0233e7cde0e2d16c1a0852f38acc25977bb
Tristan <tristan.reig@gmail.com>
2026-03-15 12:03:05 +0100

    Ajout deploiement github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://tristan-reig.github.io',
+  base: '/Portfolio',
   integrations: [react()],
-
   vite: {
     plugins: [tailwindcss()]
   }
